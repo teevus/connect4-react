635e177a44619949008dc1ea71a024e387c9270b
Matto <mac@Macbooks-MacBook.local>
2018-05-05 15:02:37 +0700

    Fixing compile errors after refactoring

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Board from './Board';
-import GameStatus from './GameStatus';
 import Constants from './Constants';
+
+const GameStatus = (props) => {
+  
+  if (props.winner > 0) {
+    return (
+    		<div>
+        	<p>{Constants.colors[props.currentPlayer]} Player has won the game!</p>
+        	<button onClick={props.onRestartClicked}>Play Again</button>
+        </div>
+    );
+  }
+  if (props.currentPlayer === -1) {
+  	return (
+    	<div>
+    		<p>Game Over!</p>
+      	<button onClick={props.onRestartClicked}>Restart Game</button>
+      </div>
+      );
+  }
+  else {
+  	return ( 
+    	<p>{Constants.colors[props.currentPlayer]} Player turn</p>
+    );
+  }
+}
+GameStatus.propTypes = {
+  currentPlayer: PropTypes.number.isRequired,
+  onRestartClicked: PropTypes.func.isRequired,
+  winner: PropTypes.number,
+};
 
 export default class Game extends React.Component {
 
